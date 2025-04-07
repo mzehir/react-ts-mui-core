2658f41c72a41383c3f6ecefa4ba9a8efa8ce7c9
Mümin <zehir.m.54@gmail.com>
2025-04-07 19:18:50 +0300

    ficed

--- src/components/base/dataGrid/DataGrid.tsx
+++ src/components/base/dataGrid/DataGrid.tsx
@@ -1,8 +1,8 @@
 import useLanguageContext from '../../../hooks/useLanguageContext';
 import { getLocaleText } from '../../../utils/locale/dataGridLocales';
 import { Language } from '../../../utils/enums/languages';
-import { DataGrid } from '@mui/x-data-grid';
 import { DataGridCompProps, dataGridCompDefaultProps } from './dataGridHelper';
+import { DataGrid } from '@mui/x-data-grid';
 
 const DataGridComp = (props: DataGridCompProps) => {
   const { ...dataGridProps } = { ...dataGridCompDefaultProps, ...props };
