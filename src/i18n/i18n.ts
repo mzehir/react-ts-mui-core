7adea72ff7bccb6fcf829aa4aca6e9e63e9374de
Mümin <zehir.m.54@gmail.com>
2025-04-09 20:32:37 +0300

    fixed

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,12 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import tr from './locales/tr.json';
 import en from './locales/en.json';
+import tr from './locales/tr.json';
 import de from './locales/de.json';
+// import enIntroductionPart1 from './locales/en/introduction/part1.json';
+// import enIntroductionPart2 from './locales/en/introduction/part2.json';
+// import enIntroductionPart3 from './locales/en/introduction/part3.json';
 
 const resources = {
   en: {
-    translation: en,
+    translation: {
+      ...en,
+      // introduction: {
+      //   part1: enIntroductionPart1,
+      //   part2: enIntroductionPart2,
+      //   part3: enIntroductionPart3,
+      // },
+    },
   },
   tr: {
     translation: tr,
@@ -17,9 +27,9 @@ const resources = {
 };
 
 i18n.use(initReactI18next).init({
+  resources,
   lng: 'en',
   fallbackLng: 'en',
-  resources: resources,
   interpolation: {
     escapeValue: false,
   },
