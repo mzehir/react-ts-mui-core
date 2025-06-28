6df95775d0c6ce989292d0c42a71ba8de760e793
Mümin <zehir.m.54@gmail.com>
2025-06-28 14:36:55 +0300

    fixed

--- src/router/helper/otherRouter.ts
+++ src/router/helper/otherRouter.ts
@@ -1,42 +1,20 @@
 import { RouterItem } from '../routerConstant';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import MoneyIcon from '@mui/icons-material/Money';
-import CallIcon from '@mui/icons-material/Call';
 
 type OtherRouterItems = {
   DASHBOARD_PAGE: RouterItem;
-  CONTACT_PAGE: RouterItem;
-  PRICING_PAGE: RouterItem;
   [key: string]: RouterItem;
 };
 
 export const OTHER_ROUTER_ITEMS: OtherRouterItems = {
   DASHBOARD_PAGE: {
-    whoCanSeeForSidebar: ['admin', 'teacher', 'student'],
-    whoCanSeeForPage: ['admin', 'teacher', 'student'],
+    whoCanSeeForSidebar: ['admin'],
+    whoCanSeeForPage: ['admin'],
     parentKey: '',
     key: 'DASHBOARD_PAGE',
     label: 'routes.dashboard',
     path: '/',
     icon: DashboardIcon,
     order: 0,
-  },
-  PRICING_PAGE: {
-    whoCanSeeForSidebar: ['admin', 'teacher', 'student'],
-    whoCanSeeForPage: ['admin', 'teacher', 'student'],
-    parentKey: '',
-    key: 'PRICING_PAGE',
-    label: 'introductionRoutes.pricing',
-    path: '/pricing',
-    icon: MoneyIcon,
-  },
-  CONTACT_PAGE: {
-    whoCanSeeForSidebar: ['admin', 'teacher', 'student'],
-    whoCanSeeForPage: ['admin', 'teacher', 'student'],
-    parentKey: '',
-    key: 'CONTACT_PAGE',
-    label: 'introductionRoutes.contact',
-    path: '/contact',
-    icon: CallIcon,
   },
 };
